be564169599c02a72896239402dd91290d26b98e
EC2 Default User <ec2-user@ip-172-31-37-54.ap-northeast-1.compute.internal>
2022-10-09 06:02:50 +0000

    rails Ver.up

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,9 @@
 class Admin::CustomersController < ApplicationController
- def new
-  @customer = Customer.new
+ def index
+    @customers = Customer.page(params[:page])
+ end
+ 
+ def update
+    @customer.update(customer_params) ? (redirect_to admin_customer_path(@customer)) : (render :edit)
  end
 end
