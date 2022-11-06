919a2a25e52c495f1428608c941e263de84947a0
masakatsukoga <mmhhr.since2014@gmail.com>
2022-11-06 08:04:41 +0000

    コントローラー変更

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,28 +1,31 @@
 class Public::CustomersController < ApplicationController
- 
+
  def new
   @customer = Customer.new
  end
- 
+
  def show
   @customer = current_customer
+  if current_customer == nil
+   redirect_to about_path
+  end
  end
- 
+
  def edit
   @customer = Customer.find(params[:id])
  end
- 
+
  def create
   @customer = Customer.new(customer_params)
   @customer.save
  end
- 
+
  def update
   @customer = Customer.find(params[:id])
   @customer.update(customer_params)
   redirect_to customer_path(@customer.id)
  end
- 
+
  private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :email,:is_deleted, :title,:content,:start_time)
