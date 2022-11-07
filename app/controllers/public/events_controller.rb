3bc53b42e0a2d05feba4c7da9f532a4dbb1f37a7
masakatsukoga <mmhhr.since2014@gmail.com>
2022-11-07 12:20:03 +0000

    groupの記述変更

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -4,17 +4,17 @@ class Public::EventsController < ApplicationController
     grouped_events = group ? group.events : [] # 自分が所属しているグル-プの全てのイベントを定義
     @events = current_customer.events #.where(id: grouped_events.ids) #グループのイベントとログインしているカスタマーが作成したイベントを繋ぐ
   end
-  
+
   def new
     @event = Event.new
   end
-  
+
   def create
     @event = Event.new(event_params)
     @event.customer_id = current_customer.id
     if @event.save
       if @event.group_id
-        redirect_to events_path
+        redirect_to group_path(@event.group_id)
       else
         redirect_to events_path
       end
@@ -22,21 +22,21 @@ class Public::EventsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @event = Event.find(params[:id])
   end
-  
+
   def update
     @event = Event.find(params[:id])
-    @event.update(event_params) 
-    if params[:event][:group_id] == "false"
+    @event.update(event_params)
+    if @event.group_id.nil?
      redirect_to events_path
     else
      redirect_to group_path(@event.group_id)
     end
   end
-  
+
   private
 
   def event_params
