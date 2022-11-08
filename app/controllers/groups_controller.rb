5866c31006bc8b22588c2324c7e2679432ae4247
masakatsukoga <mmhhr.since2014@gmail.com>
2022-11-08 13:07:50 +0000

    デプロイ完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,37 +1,37 @@
 class GroupsController < ApplicationController
   before_action :authenticate_customer!
-  
+
       def new
         @group = Group.new
       end
-      
+
       def index
         @groups = Group.all
         @groups = Group.page(params[:page])
       end
-    
+
 
       def show
         @group = Group.find(params[:id])
         @events = @group.events
         @event = Event.new
       end
-      
+
       def edit
         @group = Group.find(params[:id])
       end
-     
+
       def create
         @group = Group.new(group_params)
         @group.owner_id = current_customer.id
         if @group.save
           current_customer.update(group_id: @group.id)
-          redirect_to groups_path
+          redirect_to group_path(@group.id)
         else
           render 'new'
         end
       end
-    
+
       def update
        @group = Group.find(params[:id])
        @group.customers << current_customer
@@ -41,7 +41,7 @@ class GroupsController < ApplicationController
           render "edit"
         end
       end
-      
+
       def join
         @group = Group.find(params[:group_id])
         if @group.password_digest == (params[:group][:password])
@@ -50,26 +50,26 @@ class GroupsController < ApplicationController
         else
           redirect_to groups_path
         end
-        
+
       end
-        
+
         def disjoin
          @group = Group.find(params[:group_id])
          @group.customers.delete(current_customer)
          redirect_to groups_path
         end
-        
+
         def destroy
           @group = Group.find(params[:id])
           @group.destroy
           redirect_to groups_path
         end
-        
-    
+
+
       private
-    
+
       def group_params
         params.require(:group).permit(:name, :introduction, :password_digest)
       end
-      
+
 end
