741f524fc0b5610c4005a762dacc2d1de3631ac6
masakatsukoga <mmhhr.since2014@gmail.com>
2022-11-11 14:01:44 +0000

    イベント削除機能追加

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -37,6 +37,12 @@ class Public::EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
 
   def event_params
