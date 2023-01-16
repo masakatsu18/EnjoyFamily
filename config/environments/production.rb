7e0e6a0a72e9555e449efe2dfb31eeb802da51e2
masakatsukoga <mmhhr.since2014@gmail.com>
2023-01-16 13:03:53 +0000

    本番環境エラーログ表示

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
@@ -95,4 +95,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.consider_all_requests_local       = true
 end
