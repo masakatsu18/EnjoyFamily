f87d572d1b452d9889da4e080148466e13632cef
masakatsukoga <mmhhr.since2014@gmail.com>
2023-01-19 11:16:50 +0000

    エラーメッセージ消去

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.action_controller.perform_caching = true
-　config.consider_all_requests_local       = true
+　#config.consider_all_requests_local       = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
